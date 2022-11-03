ea3f5774ac6987ebbc0b4bcc1b756aaeb570dbcf
Fabricio Pereira da Conceição Maria <fpcmaria@apoioprodesp.sp.gov.br>
2022-11-03 14:35:36 -0300

    Operadores

--- Fundamentos/HelloWorld/HelloWorld/Program.cs
+++ Fundamentos/HelloWorld/HelloWorld/Program.cs
@@ -6,27 +6,38 @@ namespace HelloWorld
     {
         static void Main(string[] args) // Metódo de inicialização da aplicação
         {
-            char status = 'A'; //Armazena apenas um caractere
-            bool ativo = true; //Verdadeiro/falso
-            byte numero = 1;// 0 a 255;
-            sbyte negativo = -1; // -128 até 127
-            short valoresNegativoPequeno = -318;
-            ushort valoresPositivoPequeno = 318;
-            int ValoresNegativo = -39299;
-            uint ValoresPositivo = 381931031;
-            decimal temperatura = 2.0m;
-            //double temperatura = 30.0;
-            //float temperatura = 20.14f;
+            var x = 10;
+            var y = 5;
 
-            WriteLine(status.GetType()); // Mostra o tipo da váriavel
-            WriteLine(ValoresNegativo.GetType().IsPrimitive); // Verifica se é um tipo primitivo
+            var soma = x + y;
+            var subtrair = x - y;
+            var multiplicar = x * y;
+            var dividir = x / y;
+            
 
+            soma += subtrair;
+            soma -= subtrair;
+            soma *= subtrair;
+            soma /= subtrair;
+            soma %= subtrair;
 
-            int n1 = 10;
-            long n2 = n1;
-            short n3 = (short)n1; //Necessário converter de forma explicita
-            WriteLine(n2);
-            WriteLine(n3);
+            var resto = 7 % 3; // Pega o resto da divisão
+
+            WriteLine(soma);
+            WriteLine(subtrair);
+            WriteLine(multiplicar);
+            WriteLine(dividir);
+            WriteLine(resto);
+
+            var i = 0;
+
+            //Pós-incrementado
+            i++;
+            i--;
+
+            //Pré-incrementado
+            ++i;
+            --i;
         }
     }
 }
