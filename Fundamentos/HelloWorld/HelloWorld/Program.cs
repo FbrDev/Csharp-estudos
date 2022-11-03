e27309ec499f0e303adfca6f99f27e98d2cc9429
Fabricio Pereira da Conceição Maria <fpcmaria@apoioprodesp.sp.gov.br>
2022-11-03 11:49:57 -0300

    Primeira aplicação

---
+++ Fundamentos/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console; // Importação de bibliotecas
+
+namespace HelloWorld
+{
+    internal class Program // Programa Inicial
+    {
+        static void Main(string[] args) // Metódo de inicialização da aplicação
+        {
+            WriteLine("Hello, World!"); // Comando para exibir informações na tela
+
+            ReadKey(); // Espera um comando do usuário para fechar o console
+        }
+    }
+}
