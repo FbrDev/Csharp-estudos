790f42c58b90bdce8b455d8fceb48b361d91a614
Fabricio Pereira da Conceição Maria <fpcmaria@apoioprodesp.sp.gov.br>
2022-11-03 12:11:46 -0300

    Variáveis

---
+++ Fundamentos/HelloWorld/HelloWorld/Aula01HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+namespace HelloWorld
+{
+    internal class Aula01HelloWorld
+    {
+        static void Aula01()
+        {
+            WriteLine("Hello, World!"); // Comando para exibir informações na tela
+
+            ReadKey(); // Espera um comando do usuário para fechar o console
+        }
+    }
+}
--- Fundamentos/HelloWorld/HelloWorld/Program.cs
+++ Fundamentos/HelloWorld/HelloWorld/Program.cs
@@ -6,9 +6,22 @@ namespace HelloWorld
     {
         static void Main(string[] args) // Metódo de inicialização da aplicação
         {
-            WriteLine("Hello, World!"); // Comando para exibir informações na tela
+            int idade = 21;
+            string nome = "Fabrício Pereira";
+            bool ativo = false;
+            decimal salario = 1345.98m;
+            float descontos = 397.99f;
+            var cidade = "São Paulo";
 
-            ReadKey(); // Espera um comando do usuário para fechar o console
+            WriteLine($"O usuário {nome} tem {idade} anos de idade, está {ativo} no sistema ganha {salario} por mês. Tem desconto de {descontos} e mora na cidade de {cidade}");
+            WriteLine("\n-------------------------\n");
+
+            dynamic x = "Essa variável é uma string";
+            WriteLine(x);
+            x = 10;
+            WriteLine(x);
+            x = true;
+            WriteLine(x);
         }
     }
 }
