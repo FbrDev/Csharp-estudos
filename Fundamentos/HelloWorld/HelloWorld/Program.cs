2047a5245237b399ddd78d0ac988fac43b215e30
Fabricio Pereira da Conceição Maria <fpcmaria@apoioprodesp.sp.gov.br>
2022-11-03 12:29:08 -0300

    Tipos de dados

--- Fundamentos/HelloWorld/HelloWorld/Program.cs
+++ Fundamentos/HelloWorld/HelloWorld/Program.cs
@@ -6,22 +6,27 @@ namespace HelloWorld
     {
         static void Main(string[] args) // Metódo de inicialização da aplicação
         {
-            int idade = 21;
-            string nome = "Fabrício Pereira";
-            bool ativo = false;
-            decimal salario = 1345.98m;
-            float descontos = 397.99f;
-            var cidade = "São Paulo";
+            char status = 'A'; //Armazena apenas um caractere
+            bool ativo = true; //Verdadeiro/falso
+            byte numero = 1;// 0 a 255;
+            sbyte negativo = -1; // -128 até 127
+            short valoresNegativoPequeno = -318;
+            ushort valoresPositivoPequeno = 318;
+            int ValoresNegativo = -39299;
+            uint ValoresPositivo = 381931031;
+            decimal temperatura = 2.0m;
+            //double temperatura = 30.0;
+            //float temperatura = 20.14f;
 
-            WriteLine($"O usuário {nome} tem {idade} anos de idade, está {ativo} no sistema ganha {salario} por mês. Tem desconto de {descontos} e mora na cidade de {cidade}");
-            WriteLine("\n-------------------------\n");
+            WriteLine(status.GetType()); // Mostra o tipo da váriavel
+            WriteLine(ValoresNegativo.GetType().IsPrimitive); // Verifica se é um tipo primitivo
 
-            dynamic x = "Essa variável é uma string";
-            WriteLine(x);
-            x = 10;
-            WriteLine(x);
-            x = true;
-            WriteLine(x);
+
+            int n1 = 10;
+            long n2 = n1;
+            short n3 = (short)n1; //Necessário converter de forma explicita
+            WriteLine(n2);
+            WriteLine(n3);
         }
     }
 }
