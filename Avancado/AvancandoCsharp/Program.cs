be8a87c413c2620f9d8c028008f3b2bc40277b2a
Fabricio Pereira da Conceição Maria <fpcmaria@apoioprodesp.sp.gov.br>
2022-11-08 12:41:34 -0300

    Trabalhando com strings - funções de string

--- Avancado/AvancandoCsharp/Program.cs
+++ Avancado/AvancandoCsharp/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace AvancandoCsharp
+﻿using System.Globalization;
+
+namespace AvancandoCsharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string frase = "Hoje à noite, sem luz, decidi xeretar a quinta gaveta de vovô: achei linguiça, pão e fubá";
+
+            Console.WriteLine(frase.Length); // Conta quantos caracteres
+
+            string[] partes = frase.Split(","); // Quebra a frase em pedaços com base em um caractere
+            for(var i = 0; i < partes.Length; i++)
+            {
+                Console.WriteLine(partes[i].Trim()); // Retira os espaços
+            }
+
+            string subFrase = "quinta";
+            Console.WriteLine(frase.Contains(subFrase)); // retorna um boolean caso encontre a frase
+
+            int pos = frase.IndexOf(subFrase); // Mostra a posição da palavra
+
+            string sub = frase.Substring(pos);
+            Console.WriteLine(sub);
+
+            Console.WriteLine(frase.ToUpper());
+            Console.WriteLine(frase.ToLower());
+
+            string fraseTrocada = frase.Replace("Vovô", "mamãe", true, CultureInfo.CurrentCulture);
+            Console.WriteLine(fraseTrocada.Trim());
+
         }
     }
 }
