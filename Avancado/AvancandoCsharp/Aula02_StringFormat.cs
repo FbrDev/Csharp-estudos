cd7da1e55418e90d0a59771bf585c8e395913d7e
Fabricio Pereira da Conceição Maria <fpcmaria@apoioprodesp.sp.gov.br>
2022-11-09 17:21:13 -0300

    String Builder

---
+++ Avancado/AvancandoCsharp/Aula02_StringFormat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvancandoCsharp
+{
+    internal class Aula02_StringFormat
+    {
+        static void Aula02()
+        {
+            string nome = "Fabricio";
+            int idade = 21;
+            double rendimento = 433012.02;
+            DateTime dataCadastro = new DateTime(2000, 04, 02);
+
+            string frase = "O cliente {0} tem {1} anos de idade, o redimento dele é {2:c} ";
+            frase += "é cliente desde de {3:dd/MM/yyyy}";
+
+            frase = String.Format(frase, nome, idade, rendimento, dataCadastro);
+
+            Console.WriteLine(frase);
+        }
+    }
+}
--- Avancado/AvancandoCsharp/Program.cs
+++ Avancado/AvancandoCsharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Text;
 
 namespace AvancandoCsharp
 {
@@ -6,17 +7,23 @@ namespace AvancandoCsharp
     {
         static void Main(string[] args)
         {
+            StringBuilder builder = new StringBuilder("Adicionando uma frase inicial");
+            builder.Append("Adicionando mais uma frase na mesma linha");
+            builder.AppendLine("Adicionando frase com quebra linha");
+            builder.Append("Adicionando frase após o quebra linha");
+
+            Console.WriteLine(builder.ToString());
+
             string nome = "Fabricio";
             int idade = 21;
             double rendimento = 433012.02;
-            DateTime dataCadastro = new DateTime(2000,04,02);
+            DateTime dataCadastro = new DateTime(2000, 04, 02);
 
-            string frase = "O cliente {0} tem {1} anos de idade, o redimento dele é {2:c} ";
-            frase += "é cliente desde de {3:dd/MM/yyyy}";
+            StringBuilder frase = new StringBuilder("\n\nO cliente {0} tem {1} anos de idade, o redimento dele é {2:c} ");
+            frase.Append("é cliente desde de {3:dd/MM/yyyy}");
+            builder.AppendFormat(frase.ToString(), nome, idade, rendimento, dataCadastro);
 
-            frase = String.Format(frase, nome, idade, rendimento, dataCadastro);
-
-            Console.WriteLine(frase);
+            Console.WriteLine(builder.ToString());
         }
     }
 }
