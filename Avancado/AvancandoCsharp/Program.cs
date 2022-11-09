93bb3d880b3452c8ffb57192be28cabf522bc1a8
Fabricio Pereira da Conceição Maria <fpcmaria@apoioprodesp.sp.gov.br>
2022-11-09 17:47:27 -0300

    Formatando datas para strings

--- Avancado/AvancandoCsharp/Program.cs
+++ Avancado/AvancandoCsharp/Program.cs
@@ -7,9 +7,14 @@ namespace AvancandoCsharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0}", DateTime.Now); // Data atual
-            Console.WriteLine("{0}", DateTime.Now.AddDays(10)); //Valor positivo adiciona e negativo subtrai
-            Console.WriteLine("{0}", DateTime.Now);
+            string dataDoBanco = "2018-04-02 08:30:00";
+            //DateTime data = new DateTime(2018, 04, 02, 08, 30, 00);
+            DateTime data = DateTime.Parse(dataDoBanco);
+
+            string horaFormatada = String.Format("{0:HH}h{0:mm}", data);
+            string dataFormatada = String.Format("{0:dd}/{0:MM}/{0:yyyy}", data);
+
+            Console.WriteLine(dataFormatada);
         }
     }
 }
